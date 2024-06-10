66f0ae07fcfdaf5eed34bc983be9491955c88a87
ermalsalihaj <ermalsalihaj03@gmail.com>
2024-06-10 22:08:22 +0200

    Section Care, Banner, Products, Contact & Footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -44,3 +44,57 @@ let swiperFavorites = new Swiper(".favorites__swiper", {
   slidesPerView: "auto",
   centeredSlides: "auto",
 });
+
+/*=============== SHOW SCROLL UP ===============*/
+const scrollUp = () => {
+  const scrollUp = document.getElementById("scroll-up");
+  // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+  this.scrollY >= 350
+    ? scrollUp.classList.add("show-scroll")
+    : scrollUp.classList.remove("show-scroll");
+};
+window.addEventListener("scroll", scrollUp);
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll("section[id]");
+
+const scrollActive = () => {
+  const scrollDown = window.scrollY;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id"),
+      sectionsClass = document.querySelector(
+        ".nav__menu a[href*=" + sectionId + "]"
+      );
+
+    if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+      sectionsClass.classList.add("active-link");
+    } else {
+      sectionsClass.classList.remove("active-link");
+    }
+  });
+};
+window.addEventListener("scroll", scrollActive);
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2500,
+  delay: 300,
+  // reset: true, // Animations repeat
+});
+
+sr.reveal(`.home__data, .favorites__container, .footer__container`);
+sr.reveal(`.home__circle, .home__img`, { delay: 600, scale: 0.5 });
+sr.reveal(`.home__chips-1, .home__chips-2, .home__chips-3`, {
+  delay: 1000,
+  interval: 100,
+});
+sr.reveal(`.home__leaf`, { delay: 1200 });
+sr.reveal(`.home__tomato-1, .home__tomato-2`, { delay: 1400, interval: 100 });
+sr.reveal(`.care__img, .contact__img`, { origin: "left" });
+sr.reveal(`.care__list, .contact__data`, { origin: "right" });
+sr.reveal(`.banner__item, .products__card`, { interval: 100 });
